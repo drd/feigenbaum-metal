d4b4f25ab90923268ca1a9586990e985a842974f
Eric O'Connell <eric@premise.com>
2018-09-22 14:30:32 -0700

    Initial Commit

---
+++ feigenbaum/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  feigenbaum
+//
+//  Created by Eric O'Connell on 9/22/18.
+//  Copyright © 2018 Eric O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ feigenbaum/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  feigenbaum
+//
+//  Created by Eric O'Connell on 9/22/18.
+//  Copyright © 2018 Eric O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
